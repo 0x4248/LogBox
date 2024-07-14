d5f2a7afafe0bd0eec17f18e5f1acca311c9578d
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-07-14 22:08:09 +0100

    Move colours
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- LogBox/__main__.py
+++ LogBox/__main__.py
@@ -9,22 +9,7 @@ import sys
 import threading
 import importlib
 
-class Colours:
-    RED = "\033[91m"
-    GREEN = "\033[92m"
-    YELLOW = "\033[93m"
-    BLUE = "\033[94m"
-    PURPLE = "\033[95m"
-    CYAN = "\033[96m"
-    WHITE = "\033[97m"
-    RESET = "\033[0m"
-    BACK_RED = "\033[41m"
-    BACK_GREEN = "\033[42m"
-    BACK_YELLOW = "\033[43m"
-    BACK_BLUE = "\033[44m"
-    BACK_PURPLE = "\033[45m"
-    BACK_CYAN = "\033[46m"
-    BACK_WHITE = "\033[47m"
+from lib.colour import Colours
 
 __MAIN__PATH = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(__MAIN__PATH)
